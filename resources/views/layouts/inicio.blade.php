f70eb448d429d102ec4ebe71e5741a7c81967082
Daniel Oñate <daoc1094@gmail.com>
2020-07-09 20:04:55 -0500

    Template Inicio

--- resources/views/layouts/inicio.blade.php
+++ resources/views/layouts/inicio.blade.php
@@ -81,12 +81,6 @@
               <span class="nav-link-inner--text d-lg-none">Instagram</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link nav-link-icon" href="https://twitter.com/creativetim" target="_blank" data-toggle="tooltip" data-original-title="Síguenos en Twitter">
-              <i class="fab fa-twitter-square"></i>
-              <span class="nav-link-inner--text d-lg-none">Twitter</span>
-            </a>
-          </li>
         </ul>
       </div>
     </div>

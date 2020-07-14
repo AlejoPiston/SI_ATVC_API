67e0b27779cd2cd8228799ee0dceb6458352f0f4
Daniel Oñate <daoc1094@gmail.com>
2020-07-14 16:21:30 -0500

    Inicio

---
+++ resources/views/includes/panel/menu.blade.php
@@ -0,0 +1,59 @@
+<!-- Nav items -->
+<h6 class="navbar-heading p-0 text-muted">
+    <span class="docs-normal">Gestionar datos</span>
+</h6>
+<ul class="navbar-nav">
+<li class="nav-item">
+    <a class="nav-link active" href="examples/dashboard.html">
+    <i class="ni ni-tv-2 text-primary"></i>
+    <span class="nav-link-text">Dashboard</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="examples/icons.html">
+    <i class="ni ni-planet text-orange"></i>
+    <span class="nav-link-text">Órdenes de trabajo</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="examples/map.html">
+    <i class="ni ni-pin-3 text-primary"></i>
+    <span class="nav-link-text">Instalaciones</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="examples/profile.html">
+    <i class="ni ni-single-02 text-yellow"></i>
+    <span class="nav-link-text">Sistema experto</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="examples/tables.html">
+    <i class="ni ni-bullet-list-67 text-default"></i>
+    <span class="nav-link-text">Notificaciones</span>
+    </a>
+</li>
+
+
+</ul>
+<!-- Divider -->
+<hr class="my-3">
+<!-- Heading -->
+<h6 class="navbar-heading p-0 text-muted">
+<span class="docs-normal">Reportes</span>
+</h6>
+<!-- Navigation -->
+<ul class="navbar-nav mb-md-3">
+<li class="nav-item">
+    <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html" target="_blank">
+    <i class="ni ni-spaceship"></i>
+    <span class="nav-link-text">Frecuencia Órdenes de trabajo</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
+    <i class="ni ni-palette"></i>
+    <span class="nav-link-text">Técnicos más activos</span>
+    </a>
+</li>
+</ul>

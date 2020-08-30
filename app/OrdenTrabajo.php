cb9345304b083df975663186522cc5532cb48274
Daniel Oñate <daoc1094@gmail.com>
2020-08-30 13:53:21 -0500

    formato am pm ´rdenes de trabajo

--- app/OrdenTrabajo.php
+++ app/OrdenTrabajo.php
@@ -48,8 +48,8 @@ class OrdenTrabajo extends Model
     }
 
     protected $casts = [
-        'created_at' => 'datetime:d/m/Y H:i:s', // Change your format
-        'updated_at' => 'datetime:d/m/Y H:i:s',
+        'created_at' => 'datetime:d/m/Y H:00 A', // Change your format
+        'updated_at' => 'datetime:d/m/Y H:00 A',
     ];
 
 }
